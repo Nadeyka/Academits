d7781c3093e55dcd7d091b4b64e94da5754fdc1f
admintmp <admintmp@VISTUNOV-A-BOOK>
2018-09-30 21:39:08 +0700

    Добавлен класс Range

---
+++ CourseTasks/Range/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Range
+{
+    class Program
+    {
+        static void Main()
+        {
+            Range range = new Range(-0.1, 25);
+            double length = range.CalcIntervalLength();
+            Console.WriteLine("Длина интервала: " + length);
+            double number = -2.6;
+            bool isInInterval = range.IsInside(number);
+            if (isInInterval)
+            {
+                Console.WriteLine("Число {0} входит в интервал {1} - {2}", number, range.From, range.To);
+            }
+            else
+            {
+                Console.WriteLine("Число {0} не входит в интервал {1} - {2}", number, range.From, range.To);
+            }
+        }
+    }
+}
---
+++ CourseTasks/Range/Range.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Range
+{
+    class Range
+    {
+        public double From { get; set; }
+        public double To { get; set; }
+
+        public Range(double from, double to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public double CalcIntervalLength()
+        {
+            return To - From;
+        }
+
+        public bool IsInside(double number)
+        {
+            bool isInside = false;
+            if (number <= To && number >= From)
+            {
+                isInside = true;
+            }
+            return isInside;
+        }
+    }
+}
