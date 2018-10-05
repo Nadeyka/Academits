b2257bfe44e216dd43bc61263f099ee7be58703c
admintmp <admintmp@VISTUNOV-A-BOOK>
2018-10-05 20:39:30 +0700

    Задача Range*

--- CourseTasks/Range/Program.cs
+++ CourseTasks/Range/Program.cs
@@ -19,6 +19,44 @@ namespace Range
             {
                 Console.WriteLine("Число {0} не входит в интервал {1} - {2}", number, range.From, range.To);
             }
+
+            Range r1 = new Range(-200, 220);
+            Range r2 = new Range(200, 550);
+            Console.WriteLine("Исходный интервал 1: {0} ... {1}", r1.From, r1.To);
+            Console.WriteLine("Исходный интервал 2: {0} ... {1}", r2.From, r2.To);
+
+            Range intersectRange = Range.GetIntersect(r1, r2);
+            if (intersectRange != null)
+            {
+                Console.WriteLine("Интервал пересечения: {0} ... {1}", intersectRange.From, intersectRange.To);
+            }
+            else
+            {
+                Console.WriteLine("Интервалы не пересекаются");
+            }
+
+            Range[] unionRange = Range.GetUnion(r1, r2);
+            if (unionRange[1] != null)
+            {
+                Console.WriteLine("Интервал объединения 1: {0} ... {1}", unionRange[0].From, unionRange[0].To);
+                Console.WriteLine("Интервал объединения 2: {0} ... {1}", unionRange[1].From, unionRange[1].To);
+            }
+            else
+            {
+                Console.WriteLine("Интервал объединения: {0} ... {1}", unionRange[0].From, unionRange[0].To);
+            }
+
+            Range[] differenceRange = Range.GetDifference(r1, r2);
+            if (differenceRange[1] != null)
+            {
+                Console.WriteLine("Интервал разности 1: {0} ... {1}", differenceRange[0].From, differenceRange[0].To);
+                Console.WriteLine("Интервал разности 2: {0} ... {1}", differenceRange[1].From, differenceRange[1].To);
+            }
+            else
+            {
+                Console.WriteLine("Интервал разности: {0} ... {1}", differenceRange[0].From, differenceRange[0].To);
+            }
+
         }
     }
 }
--- CourseTasks/Range/Range.cs
+++ CourseTasks/Range/Range.cs
@@ -27,5 +27,101 @@ namespace Range
         {
             return (number <= To && number >= From);
         }
+
+        public static Range GetIntersect(Range r1, Range r2)
+        {
+            Range intersectRange = new Range(r1.From, r1.To);
+            if (r1.To < r2.From)
+            {
+                return null;
+            }
+            if (r1.To >= r2.From && r1.From <= r2.From)
+            {
+                intersectRange.From = r2.From;
+                intersectRange.To = r1.To;
+            }
+            if (r1.From >= r2.From && r1.To <= r2.To)
+            {
+                intersectRange.From = r1.From;
+                intersectRange.To = r1.To;
+            }
+            if (r1.From <= r2.From && r1.To >= r2.To)
+            {
+                intersectRange.From = r2.From;
+                intersectRange.To = r2.To;
+            }
+            if (r1.From >= r2.From && r1.To >= r2.To)
+            {
+                intersectRange.From = r1.From;
+                intersectRange.To = r2.To;
+            }
+            return intersectRange;
+        }
+
+        public static Range[] GetUnion(Range r1, Range r2)
+        {
+            Range[] unionRangeArray = new Range[2];
+            unionRangeArray[0] = r1;
+            // Range unionRange = new Range(r1.From, r1.To);
+            if (r1.To < r2.From)
+            {
+                unionRangeArray[0] = r1;
+                unionRangeArray[1] = r2;
+            }
+            if (r1.To >= r2.From && r1.From <= r2.From)
+            {
+                unionRangeArray[0].From = r1.From;
+                unionRangeArray[0].To = r2.To;
+            }
+            if (r1.From >= r2.From && r1.To <= r2.To)
+            {
+                unionRangeArray[0].From = r2.From;
+                unionRangeArray[0].To = r2.To;
+            }
+            if (r1.From <= r2.From && r1.To >= r2.To)
+            {
+                unionRangeArray[0].From = r1.From;
+                unionRangeArray[0].To = r1.To;
+            }
+            if (r1.From >= r2.From && r1.To >= r2.To)
+            {
+                unionRangeArray[0].From = r2.From;
+                unionRangeArray[0].To = r1.To;
+            }
+            return unionRangeArray;
+        }
+
+        public static Range[] GetDifference(Range r1, Range r2)
+        {
+            Range[] differenceRangeArray = new Range[2];
+            differenceRangeArray[0] = r1;
+            if (r1.To < r2.From)
+            {
+                differenceRangeArray[0] = r1;
+            }
+            if (r1.To >= r2.From && r1.From <= r2.From)
+            {
+                differenceRangeArray[0].From = r1.From;
+                differenceRangeArray[0].To = r2.From - 0.001;
+            }
+            if (r1.From >= r2.From && r1.To <= r2.To)
+            {
+                differenceRangeArray[0] = null;
+            }
+            if (r1.From <= r2.From && r1.To >= r2.To)
+            {
+                differenceRangeArray[0].From = r1.From;
+                differenceRangeArray[0].To = r2.From - 0.001;
+                differenceRangeArray[1].From = r2.To + 0.001;
+                differenceRangeArray[1].To = r1.To;
+            }
+            if (r1.From >= r2.From && r1.To >= r2.To)
+            {
+                differenceRangeArray[0].From = r2.To + 0.001;
+                differenceRangeArray[0].To = r1.To;
+            }
+            return differenceRangeArray;
+        }
+
     }
 }
