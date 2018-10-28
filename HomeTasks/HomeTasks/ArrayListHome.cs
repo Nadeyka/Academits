d89a7469b6fcd16acb967166ce66b1b56cc31128
admintmp <admintmp@VISTUNOV-A-BOOK>
2018-10-28 20:28:35 +0700

    добавила обработку исключения при работе с файлом

--- HomeTasks/HomeTasks/ArrayListHome.cs
+++ HomeTasks/HomeTasks/ArrayListHome.cs
@@ -13,61 +13,70 @@ namespace HomeTasks
         {
             #region Чтение строк из файла в список
             List<string> fileContent = new List<string>();
-            using (StreamReader sr = new StreamReader("../../input.txt"))
+            try
             {
-                string line;
-                while ((line = sr.ReadLine()) != null)
+                using (StreamReader sr = new StreamReader("../../input.txt"))
                 {
-                    fileContent.Add(line);
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        fileContent.Add(line);
+                    }
                 }
-            }
 
-            foreach (string e in fileContent)
-            {
-                Console.WriteLine(e);
-            }
-            #endregion
-
-            #region Удаление четных чисел из списка
-            List<int> intNumbersList = new List<int> { 1, 2, 3, 45, 6, 7, 51, 16 };
-            int i = 0;
-            while (i < intNumbersList.Count)
-            {
-                if (intNumbersList[i] % 2 == 0)
+                foreach (string e in fileContent)
                 {
-                    intNumbersList.RemoveAt(i);
+                    Console.WriteLine(e);
                 }
-                else
-                {
-                    i++;
-                }
-            }
-            Console.WriteLine();
-            Console.WriteLine("Список без четных чисел:");
-            foreach (int e in intNumbersList)
-            {
-                Console.Write(e + " ");
-            }
-            #endregion
+                #endregion
 
-            #region Создание копии списка без повторений
-            List<int> originalList = new List<int> { 0, 1, 2, 3, 4, 6, 1, 12, 11, 3, 6, 8 };
-            List<int> notDuplicateList = new List<int>();
-
-            foreach (int e in originalList)
-            {
-                if (!notDuplicateList.Contains(e))
+                #region Удаление четных чисел из списка
+                List<int> intNumbersList = new List<int> { 1, 2, 3, 45, 6, 7, 51, 16 };
+                int i = 0;
+                while (i < intNumbersList.Count)
                 {
-                    notDuplicateList.Add(e);
+                    if (intNumbersList[i] % 2 == 0)
+                    {
+                        intNumbersList.RemoveAt(i);
+                    }
+                    else
+                    {
+                        i++;
+                    }
                 }
+                Console.WriteLine();
+                Console.WriteLine("Список без четных чисел:");
+                foreach (int e in intNumbersList)
+                {
+                    Console.Write(e + " ");
+                }
+                #endregion
+
+                #region Создание копии списка без повторений
+                List<int> originalList = new List<int> { 0, 1, 2, 3, 4, 6, 1, 12, 11, 3, 6, 8 };
+                List<int> notDuplicateList = new List<int>();
+
+                foreach (int e in originalList)
+                {
+                    if (!notDuplicateList.Contains(e))
+                    {
+                        notDuplicateList.Add(e);
+                    }
+                }
+                Console.WriteLine();
+                Console.WriteLine("Список без повторяющихся чисел:");
+                foreach (int e in notDuplicateList)
+                {
+                    Console.Write(e + " ");
+                }
+                #endregion
+
             }
-            Console.WriteLine();
-            Console.WriteLine("Список без повторяющихся чисел:");
-            foreach (int e in notDuplicateList)
+            catch (FileNotFoundException)
             {
-                Console.Write(e + " ");
+                Console.WriteLine("Файл не найден");
+                //throw;
             }
-            #endregion
 
         }
     }
