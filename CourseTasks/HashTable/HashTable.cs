5ec2290c56b7292996b7fd8a237930944745885f
admintmp <admintmp@VISTUNOV-A-BOOK>
2018-10-29 21:03:08 +0700

    Уменьшила длину Try, сдвинула регион

---
+++ CourseTasks/HashTable/HashTable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    class HashTable
+    {
+        private List<object>[] hashTableArray;
+
+        HashTable(int tableSize)
+        {
+            hashTableArray = new List<object>[tableSize];
+        }
+
+        public void InsertObject(object objToInsert)
+        {
+            int index = objToInsert.GetHashCode();
+            
+        }
+
+        public void SearchObject(object objToSearch)
+        {
+
+        }
+    }
+}
--- HomeTasks/HomeTasks/ArrayListHome.cs
+++ HomeTasks/HomeTasks/ArrayListHome.cs
@@ -15,7 +15,8 @@ namespace HomeTasks
             List<string> fileContent = new List<string>();
             try
             {
-                using (StreamReader sr = new StreamReader("../../input.txt"))
+                StreamReader sr = new StreamReader("../../input.txt");
+                using (sr)
                 {
                     string line;
                     while ((line = sr.ReadLine()) != null)
@@ -23,61 +24,56 @@ namespace HomeTasks
                         fileContent.Add(line);
                     }
                 }
-
-                foreach (string e in fileContent)
-                {
-                    Console.WriteLine(e);
-                }
-                #endregion
-
-                #region Удаление четных чисел из списка
-                List<int> intNumbersList = new List<int> { 1, 2, 3, 45, 6, 7, 51, 16 };
-                int i = 0;
-                while (i < intNumbersList.Count)
-                {
-                    if (intNumbersList[i] % 2 == 0)
-                    {
-                        intNumbersList.RemoveAt(i);
-                    }
-                    else
-                    {
-                        i++;
-                    }
-                }
-                Console.WriteLine();
-                Console.WriteLine("Список без четных чисел:");
-                foreach (int e in intNumbersList)
-                {
-                    Console.Write(e + " ");
-                }
-                #endregion
-
-                #region Создание копии списка без повторений
-                List<int> originalList = new List<int> { 0, 1, 2, 3, 4, 6, 1, 12, 11, 3, 6, 8 };
-                List<int> notDuplicateList = new List<int>();
-
-                foreach (int e in originalList)
-                {
-                    if (!notDuplicateList.Contains(e))
-                    {
-                        notDuplicateList.Add(e);
-                    }
-                }
-                Console.WriteLine();
-                Console.WriteLine("Список без повторяющихся чисел:");
-                foreach (int e in notDuplicateList)
-                {
-                    Console.Write(e + " ");
-                }
-                #endregion
-
             }
             catch (FileNotFoundException)
             {
                 Console.WriteLine("Файл не найден");
-                //throw;
             }
+            foreach (string e in fileContent)
+            {
+                Console.WriteLine(e);
+            }
+            #endregion
 
+            #region Удаление четных чисел из списка
+            List<int> intNumbersList = new List<int> { 1, 2, 3, 45, 6, 7, 51, 16 };
+            int i = 0;
+            while (i < intNumbersList.Count)
+            {
+                if (intNumbersList[i] % 2 == 0)
+                {
+                    intNumbersList.RemoveAt(i);
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Список без четных чисел:");
+            foreach (int e in intNumbersList)
+            {
+                Console.Write(e + " ");
+            }
+            #endregion
+
+            #region Создание копии списка без повторений
+            List<int> originalList = new List<int> { 0, 1, 2, 3, 4, 6, 1, 12, 11, 3, 6, 8 };
+            List<int> notDuplicateList = new List<int>();
+            foreach (int e in originalList)
+            {
+                if (!notDuplicateList.Contains(e))
+                {
+                    notDuplicateList.Add(e);
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Список без повторяющихся чисел:");
+            foreach (int e in notDuplicateList)
+            {
+                Console.Write(e + " ");
+            }
+            #endregion
         }
     }
 }
